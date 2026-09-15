4cbc7521c3d4f55f5ddb9fa6195a789a5e4949eb
jsookun69-alt <jsookun69@gmail.com>
2026-09-15 15:59:44 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PerfumeIntelligence from './perfume-intelligence.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<PerfumeIntelligence />
+</React.StrictMode>,
+)
